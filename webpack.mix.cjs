439e191df1556046618a9ba834e7155b95cc7a9a
HUNG <hung060101@gmail.com>
2024-07-04 01:25:37 +0700

    setup react

--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -1,6 +1,6 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/react')
    .react()
    // .sass('resources/sass/app.scss', 'public/css')
    .version();
