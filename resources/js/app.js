fb1de87acd4ee01a2b113fb18eb3b39a4262f3d3
HUNG <hung060101@gmail.com>
2024-07-04 01:17:41 +0700

    setup react

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-import './bootstrap';
+import './bootstrap.js';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Example from './components/example.js';
+
+const root = ReactDOM.createRoot(document.getElementById('example'));  // Create a root for rendering
+root.render(<Example />);  // Render the Example component
---
+++ resources/js/components/example.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Example = () => {
+  return <div>Hello, React!</div>;
+};
+
+export default Example;
